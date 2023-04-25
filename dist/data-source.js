33c917a9ce219836e51ff4cf792c073019b2f626
Talles Simao <102775805+Tallesco201@users.noreply.github.com>
2023-04-25 16:07:59 -0300

    fazendo algumas alteraçoes no data-source.ts

--- dist/data-source.js
+++ dist/data-source.js
@@ -1,17 +1,23 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const typeorm_1 = require("typeorm");
+const path_1 = __importDefault(require("path"));
 require("dotenv/config");
-const projects_entity_1 = require("./entities/projects.entity");
-const _1682110508552_updateTableProject_1 = require("./migrations/1682110508552-updateTableProject");
 const nodeEnv = process.env.NODE_ENV;
-const obj = {
-    type: "postgres",
-    url: process.env.DATABASE_URL,
-    entities: [projects_entity_1.Project],
-    migrations: [_1682110508552_updateTableProject_1.UpdateTableProject1682110508552],
-};
-nodeEnv === "production" ? { obj } : {};
+const entitiesPath = path_1.default.join(__dirname, "./entities/**.{js,ts}");
+const migrationsPath = path_1.default.join(__dirname, "./migrations/**.{js,ts}");
+/* if (nodeEnv==="production"){
+    return{
+        type:"postgres",
+        url: process.env.DATABASE_URL,
+        entities: [entitiesPath],
+        migrations: [migrationsPath],
+    }
+
+} */
 const AppDataSource = new typeorm_1.DataSource({
     type: "postgres",
     host: process.env.PGHOST,
@@ -21,8 +27,8 @@ const AppDataSource = new typeorm_1.DataSource({
     database: process.env.PGDATABASE,
     logging: true,
     synchronize: false,
-    entities: [projects_entity_1.Project],
-    migrations: [_1682110508552_updateTableProject_1.UpdateTableProject1682110508552]
+    entities: [entitiesPath],
+    migrations: [migrationsPath]
 });
 exports.default = AppDataSource;
 //# sourceMappingURL=data-source.js.map
--- src/data-source.ts
+++ src/data-source.ts
@@ -5,31 +5,35 @@ import { Project } from "./entities/projects.entity"
 import { CreateTables1682109780965 } from "./migrations/1682109780965-createTables"
 import { UpdateTableProject1682110508552 } from "./migrations/1682110508552-updateTableProject"
 
+
+
 const nodeEnv: string = process.env.NODE_ENV;
 
-const obj = {
-    
-        type: "postgres",
+const entitiesPath:string = path.join(__dirname, "./entities/**.{js,ts}");
+const migrationsPath:string = path.join(__dirname, "./migrations/**.{js,ts}") 
+
+/* if (nodeEnv==="production"){
+    return{
+        type:"postgres",
         url: process.env.DATABASE_URL,
-        entities: [Project],
-        migrations: [UpdateTableProject1682110508552],
-}
+        entities: [entitiesPath],
+        migrations: [migrationsPath],
+    }
 
-nodeEnv === "production"? {obj}:{}
-
-
+} */
 
 const AppDataSource =  new DataSource({
     type:"postgres",
-    host:process.env.PGHOST,
+    url:process.env.DATABASE_URL,
+    /* host:process.env.PGHOST,
     port: parseInt(process.env.PGPORT!),
     username: process.env.PGUSER,
     password: process.env.PGPASSWORD,
-    database: process.env.PGDATABASE,
+    database: process.env.PGDATABASE, */
     logging: true,
     synchronize: false,
-    entities: [Project],
-    migrations: [UpdateTableProject1682110508552]
+    entities: [entitiesPath],
+    migrations: [migrationsPath]
 })
 
 export default AppDataSource

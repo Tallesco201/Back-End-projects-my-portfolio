28b101a9bf089a434d1c69ade8deb49a71a66865
Talles Simao <102775805+Tallesco201@users.noreply.github.com>
2023-04-28 18:17:45 -0300

    tentativa de resolver o problema CORS

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { Request, Response, NextFunction } from 'express';
 import "express-async-errors"
 import projectRouter from "./routes/projectsRoutes";
 import handleError from "./errors/handleError";
@@ -6,6 +7,13 @@ import handleError from "./errors/handleError";
 
 const app = express();
 app.use(express.json());
+
+app.use(function(req:Request, res:Response, next:NextFunction) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.use("/project", projectRouter)
 
